074bcf2547a46830405c8a4ca76a76fa9c0eab4a
Carlos <carlos.pinto@exaud.com>
2022-02-04 11:35:48 +0000

    Examined target correlation with features. Clearer prints

--- DeepLearning/main.py
+++ DeepLearning/main.py
@@ -2,4 +2,18 @@ import pandas as pd
 
 # store dataset as Data Frame (table format)
 df = pd.read_csv("DeepLearning/dataset/WineQT.csv", index_col="Id")
-print ("First 5 rows of data:\n",df.head())
+print ("\n>>> First 5 rows of data:\n\n",df.head())
+
+
+corr_dict = {
+    "feature": [],
+    "correlation": []
+}
+for corr, key in zip(df.corr().values[:,-1], df):
+    if key != "quality":
+        corr_dict["feature"].append(key)
+        corr_dict["correlation"].append(abs(corr))
+
+corr_df = pd.DataFrame(corr_dict)
+corr_df = corr_df.sort_values("correlation", ascending=False)
+print ("\n>>> Correlation between wine quality and measured features: \n\n", corr_df)
