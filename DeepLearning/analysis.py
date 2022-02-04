334511d04ef5ff12013dda45bf406fd0e3216ab0
Carlos <carlos.pinto@exaud.com>
2022-02-04 16:14:07 +0000

    visualize relationship between learning rate and score

--- DeepLearning/analysis.py
+++ DeepLearning/analysis.py
@@ -3,3 +3,10 @@ import pandas as pd
 
 res_df = pd.read_csv("DeepLearning/results.csv", index_col="id")
 print (res_df.head())
+
+import matplotlib.pyplot as plt
+
+plt.scatter(res_df["learning_rate"], res_df["mean_test_score"])
+plt.xlabel("Learning Rate")
+plt.ylabel("mean_test_score")
+plt.show()
