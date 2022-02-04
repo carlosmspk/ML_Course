4b0f203c9bccd7631bdc205d0fd9792e87db0176
Carlos <carlos.pinto@exaud.com>
2022-02-04 16:26:08 +0000

    extract function

--- DeepLearning/analysis.py
+++ DeepLearning/analysis.py
@@ -6,13 +6,14 @@ print (res_df.head())
 
 import matplotlib.pyplot as plt
 
-def plot_in_axis(axis, column_label):
-    axis.scatter(res_df[column_label], res_df["mean_test_score"])
-    axis.set_title (column_label)
+def plot_relationships (df):
+    def plot_in_axis(axis, column_label):
+        axis.scatter(df[column_label], df["mean_test_score"])
+        axis.set_title (column_label)
 
-_, axs = plt.subplots(1,3)
-for i, key in enumerate(res_df):
-    if key == "mean_test_score":
-        break
-    plot_in_axis(axs[i], key)
-plt.show()
+    _, axs = plt.subplots(1,3)
+    for i, key in enumerate(df):
+        if key == "mean_test_score":
+            break
+        plot_in_axis(axs[i], key)
+    plt.show()
