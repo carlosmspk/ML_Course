ba76f55db0dfe5d4a9b9fe568f9b7eae6922f9cd
Carlos <carlos.pinto@exaud.com>
2022-02-04 12:05:45 +0000

    imported keras packages and formatted io variables

--- DeepLearning/main.py
+++ DeepLearning/main.py
@@ -17,3 +17,13 @@ for corr, key in zip(df.corr().values[:,-1], df):
 corr_df = pd.DataFrame(corr_dict)
 corr_df = corr_df.sort_values("correlation", ascending=False)
 print ("\n>>> Correlation between wine quality and measured features: \n\n", corr_df)
+
+#### Build the Model
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+x = df.values[:,:-1]
+y = df.values[:,-1]
+
+print (x.shape, y.shape)
