5cf15dd3e027dbdd7d887a3eb33bc6480d69dd38
Carlos <carlos.pinto@exaud.com>
2022-02-07 11:28:37 +0000

    fixed output shape

--- DeepLearning/indian_food_scripts/main.py
+++ DeepLearning/indian_food_scripts/main.py
@@ -22,7 +22,7 @@ if LOAD_SMALL_SUBSET:
 
 label = 0
 x = np.zeros((n_images,300,300,3))
-y = np.zeros((n_images, n_labels))
+y = np.zeros((n_images,))
 
 i = 0
 label = 0
@@ -37,7 +37,7 @@ for dirname in listdir("dataset/IndianFood"):
             break
         loaded_images += 1
         x[i] = img.imread("dataset/IndianFood/" + dirname + "/" + image)
-        y[i][label] = 1
+        y[i] = label
         i += 1
     label += 1
     print("done!")
@@ -53,13 +53,13 @@ from keras.layers import Flatten, Dense, Dropout
 from keras.models import Sequential
 from keras.losses import SparseCategoricalCrossentropy
 
-def create_model(input_shape, output_labels) -> Sequential:
+def create_model(input_sample, label_num) -> Sequential:
     # Stack NN layers
     model = Sequential()
-    model.add(Flatten(input_shape=(input_shape)))
+    model.add(Flatten(input_shape=input_sample.shape))
     model.add(Dense(128, activation="relu"))
     model.add(Dropout(0.2))
-    model.add(Dense(output_labels))
+    model.add(Dense(label_num))
     
     # create loss function
     loss_fn = SparseCategoricalCrossentropy(from_logits=True)
@@ -69,7 +69,7 @@ def create_model(input_shape, output_labels) -> Sequential:
 
     return model
 
-model = create_model((300,300, 3), y.shape[1])
+model = create_model(x[0], n_labels)
 
 from sklearn.model_selection import train_test_split
 
