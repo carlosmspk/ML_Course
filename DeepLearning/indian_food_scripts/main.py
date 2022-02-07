73855ca532018146dc4ef263d49495d25ee4ccc2
Carlos <carlos.pinto@exaud.com>
2022-02-07 16:13:46 +0000

    deleted all data fetching code from main

--- DeepLearning/indian_food_scripts/main.py
+++ DeepLearning/indian_food_scripts/main.py
@@ -1,57 +1,18 @@
-from matplotlib import image as img
-import numpy as np
-from os import listdir, chdir
-
-LOAD_SMALL_SUBSET = True
-N_IMGS_TO_LOAD = 100
-
-chdir("DeepLearning")
-n_images = 0
-n_labels = 0
-encoder = dict()
-for dirname in listdir("dataset/IndianFood"):
-    if dirname == ".DS_Store":
-        continue
-    for image in listdir("dataset/IndianFood/" + dirname):
-        n_images += 1
-    encoder[n_labels] = dirname
-    n_labels += 1
-
-if LOAD_SMALL_SUBSET:
-    n_images = N_IMGS_TO_LOAD * n_labels
-
-label = 0
-x = np.zeros((n_images,300,300,3))
-y = np.zeros((n_images,))
-
-i = 0
-label = 0
-
-for dirname in listdir("dataset/IndianFood"):
-    if dirname == ".DS_Store":
-        continue
-    print (f"Loading {dirname}... ", end="", flush=True)
-    loaded_images = 0
-    for image in listdir("dataset/IndianFood/" + dirname):
-        if LOAD_SMALL_SUBSET and loaded_images >= N_IMGS_TO_LOAD:
-            break
-        loaded_images += 1
-        x[i] = img.imread("dataset/IndianFood/" + dirname + "/" + image)
-        y[i] = label
-        i += 1
-    label += 1
-    print("done!")
-
-print (x.shape)
-print (y.shape)
-
-
-# Start building model
-
 import tensorflow as tf
 from keras.layers import Flatten, Dense, Dropout
 from keras.models import Sequential
-from keras.losses import SparseCategoricalCrossentropy
+from batch import BatchGenerator
+import numpy as np
+
+DATASET_PATH = "DeepLearning/dataset/IndianFood/"
+TEST_SIZE = .2
+VALIDATION_SIZE = .2
+BATCH_SIZE = 60
+EPOCHS = 5
+
+batch_gen = BatchGenerator(DATASET_PATH, BATCH_SIZE)
+
+i_shape, o_shape = batch_gen.img_shape, batch_gen.output_shape[1]
 
 def create_model(input_sample, label_num) -> Sequential:
     # Stack NN layers
@@ -62,18 +23,15 @@ def create_model(input_sample, label_num) -> Sequential:
     model.add(Dense(label_num, activation="softmax"))
     
     # create loss function
-    loss_fn = SparseCategoricalCrossentropy(from_logits=True)
     model.compile(optimizer="adam",
-    loss=loss_fn,
+    loss="categorical_crossentropy",
     metrics=["accuracy"])
+
+    model.summary(line_length=42)
 
     return model
 
-model = create_model(x[0], n_labels)
+model = create_model(np.zeros(i_shape), o_shape)
 
-from sklearn.model_selection import train_test_split
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2)
-
-model.fit_generator()
+model.fit_generator(generator=)
 model.evaluate(x_test, y_test, verbose=2)
