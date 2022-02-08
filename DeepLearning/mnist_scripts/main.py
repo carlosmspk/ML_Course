d2da1a9ab5c8876e06b1f6f4ca62a1af8af44d52
Carlos Pinto <carlos.pinto@exaud.com>
2022-02-08 16:48:24 +0000

    allow main to first try to load model and data

--- DeepLearning/mnist_scripts/main.py
+++ DeepLearning/mnist_scripts/main.py
@@ -1,13 +1,28 @@
 from keras.datasets import mnist
-from utils import preprocess_data, create_model, fit_and_train, plot_results
-from pickle import load
+from utils import preprocess_data, create_model, fit_and_train, plot_results, load
+from keras.models import load_model
+from os import environ
+environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 
 if __name__ == "__main__":
 
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train, y_train, x_test, y_test = preprocess_data(x_train, y_train, x_test, y_test)
+    METRICS_PATH="DeepLearning/results/mnist_history.pkl"
+    MODEL_PATH="DeepLearning/results/mnist_last_model"
 
-    model = create_model(x_train.shape[1:], 10)
-    accuracy = fit_and_train (model, x_train, y_train, x_test, y_test, epochs=20, store_metrics="DeepLearning/results/mnist_history.pkl")
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, y_train, x_test, y_test = preprocess_data(x_train, y_train, x_test, y_test, shrink=.2)
+
+    print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
+    try:
+        model = load_model(MODEL_PATH)
+        accuracy = load(METRICS_PATH)
+        print("Found previously trained model and accuracy data.")
+    except (FileNotFoundError, OSError):
+        print("No stored model and/or accuracy values found. Creating new model...")
+        model = create_model(x_train.shape[1:], 10)
+        accuracy = fit_and_train (model, x_train, y_train, x_test, y_test, epochs=20, store_metrics=METRICS_PATH)
+        model.save(MODEL_PATH)
+
     plot_results(accuracy)
