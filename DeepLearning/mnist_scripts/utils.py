11875f74d4cc3775e17080c6be7a710a1c95f64c
Carlos Pinto <carlos.pinto@exaud.com>
2022-02-08 15:18:20 +0000

    new function: create hard coded model

--- DeepLearning/mnist_scripts/utils.py
+++ DeepLearning/mnist_scripts/utils.py
@@ -18,3 +18,40 @@ def preprocess_data(x_train, y_train, x_test, y_test):
     x_test /= 255
 
     return x_train, y_train, x_test, y_test
+
+
+def create_model(input_shape, label_num) -> Sequential:
+    model = Sequential()
+    model.add(Conv2D(filters = 64, kernel_size = (5,5), activation ='relu',input_shape=input_shape))
+    model.add(BatchNormalization(axis=3))
+    model.add(Conv2D(filters = 64, kernel_size = (5,5), activation ='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(BatchNormalization(axis=3))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(filters = 128, kernel_size = (5,5), activation ='relu'))
+    model.add(BatchNormalization(axis=3))
+    model.add(Conv2D(filters = 128, kernel_size = (5,5), activation ='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(BatchNormalization(axis=3))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+
+    model.add(Dense(64, activation = "relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+    model.add(Dense(32, activation = "relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+    model.add(Dense(label_num, activation = "softmax"))
+    
+    model.compile(optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"])
+    
+    model.summary()
+
+    return model
